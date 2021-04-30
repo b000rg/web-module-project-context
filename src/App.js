2e4dbbe567b6e85eda15562d155bfc9a699fdef1
b000rg <33711099+b000rg@users.noreply.github.com>
2021-04-29 19:50:32 -0500

    completed MVP and stretch

--- src/App.js
+++ src/App.js
@@ -1,34 +1,54 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  useEffect(() => {
+    const myCart = localStorage.getItem("myCart");
+    if (myCart) setCart(JSON.parse(myCart));
+  }, []);
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  useEffect(() => {
+    localStorage.setItem("myCart", JSON.stringify(cart));
+  }, [cart]);
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+  const addItem = (item) => {
+    // add the given item to the cart
+    setCart([...cart, item]);
+  };
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+  const removeItem = (id) => {
+    setCart(cart.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{ cart, removeItem }}>
+          <Navigation />
+
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
+
+          <Route path="/cart">
+            <ShoppingCart />
+          </Route>
+        </CartContext.Provider>
+      </ProductContext.Provider>
+    </div>
+  );
 }
 
 export default App;
